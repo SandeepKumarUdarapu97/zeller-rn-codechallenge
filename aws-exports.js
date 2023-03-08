2a14ba4e1f3f47694ac47953532fa5f05e038549
James Merritt <james.merritt@myzeller.com>
2023-03-08 18:14:42 +1100

    update API key

--- aws-exports.js
+++ aws-exports.js
@@ -4,7 +4,7 @@ const awsconfig =  {
     "aws_appsync_graphqlEndpoint": "https://prrwjjssnvhpbcdwbcwx3nm3zm.appsync-api.ap-southeast-2.amazonaws.com/graphql",
     "aws_appsync_region": "ap-southeast-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-rgx3ut2yh5a5nnmstkmvld4bgy",
+    "aws_appsync_apiKey": "da2-psmv7fcrw5dlpmp5orner2xf7i",
 };
 
 export default awsconfig;
